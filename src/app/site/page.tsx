c5fb5c2ad64614d6a5075994b9605e40194e4d37
vishwansh <165544401+vishwansh01@users.noreply.github.com>
2025-08-25 22:33:38 +0530

    Update page.tsx

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
           <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
           {/* <div className="flex justify-center mt-20"> */}
           <p className="text-center mt-20 md:pt-[100px] pt-[40vh]">
-            Run your agency, in one place
+            Run your agency, in one place 
           </p>
           {/* </div> */}
 
